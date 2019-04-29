7b374728bfc5192f52cb82cd0272c7ef72df57d3
Adetula Tolulope <adetulatolu@gmail.com>
2019-04-29 06:27:49 +0100

    User Login and Authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,59 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import auth
+
 
 # Create your views here.
 def login(request):
     if request.method == 'POST':
-        return
-    else
+        username = request.POST['username']
+        password = request.POST['password'] 
+
+        user = auth.authenticate(username = username, password = password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            return redirect('login')
+
+    else:
         return render(request, 'accounts/login.html')
 
 def logout(request):
     return redirect('index')
 
 def register(request):
+    #Get post values
     if request.method == "POST":
-        return
-    else:
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
 
+        #password validation
+
+        if password == password2:
+            if User.objects.filter(username = username).exists():
+                messages.error(request, 'That USername is Taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email = email).exists():
+                    messages.error(request, 'That Email is been used')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username,first_name=first_name,last_name=last_name,password=password,email=email)
+                    user.save()
+                    messages.success(request, 'Welcome to BTRE')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+    else:
         return render(request, 'accounts/register.html')
 
 def dashboard(request):
